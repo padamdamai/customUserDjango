5241be329568435aebcab3018641f755def8c737
Padam Damai <padamdamai888@gmail.com>
2024-04-20 16:09:32 +0545

    added email instead of username  to models.py

--- base/models.py
+++ base/models.py
@@ -2,4 +2,10 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    pass
+    name = models.CharField(max_length=200,null=True)
+    email = models.EmailField(unique=True)
+    bio = models.TextField(null=True)
+
+#tell django to replace the field
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
